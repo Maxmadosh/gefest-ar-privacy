e45ce986d4a313a52215ff10b5c5071165f238a0
Zamirbek uulu Maksatbek <liiftwall@gmail.com>
2026-03-21 22:18:33 +0600

    Initial Commit

---
+++ GefestAR/GefestARApp.swift
@@ -0,0 +1,17 @@
+//
+//  GefestARApp.swift
+//  GefestAR
+//
+//  Created by Макс Замир on 21/3/26.
+//
+
+import SwiftUI
+
+@main
+struct GefestARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
